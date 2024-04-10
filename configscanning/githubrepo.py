86de1d106d1349d8b7b7bf9b53c250a52d2c0eaf
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-04-10 09:46:32 +0100

    EODHP-246 - fix error when defining main branch

--- configscanning/githubrepo.py
+++ configscanning/githubrepo.py
@@ -57,8 +57,8 @@ class AI4DTERepo:
                              https://github-host-name/orgname/reponame.git
               branches_to_fetch (set[str]): list of branch names to fetch - these MUST exist
         """
-        if branches_to_fetch is None:
-            branches_to_fetch = {"main"}
+        if branches_to_fetch == {'main'}:
+            branches_to_fetch = {None}
 
         self.repourl = repourl
         self.repourlobj = urlparse(repourl)
